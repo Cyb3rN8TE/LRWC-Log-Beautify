9dded28ebbbd35983400b3a5cc981ecf3455708b
Cyb3rN8TE <127637597+Cyb3rN8TE@users.noreply.github.com>
2023-10-28 19:13:45 +1100

    Modified banner

--- LRWC Log Beautify.ps1
+++ LRWC Log Beautify.ps1
@@ -10,8 +10,8 @@ Write-Host -ForegroundColor White '888      888 T88b   88888P Y88888 888    888 
 Write-Host -ForegroundColor White '888      888  T88b  8888P   Y8888 Y88b  d88P      888     Y88..88P Y88b 888      888   d88P Y8b.     888  888 Y88b 888 Y88b.  888 888    Y88b 888 '
 Write-Host -ForegroundColor White '88888888 888   T88b 888P     Y888  "Y8888P"       88888888 "Y88P"   "Y88888      8888888P"   "Y8888  "Y888888  "Y88888  "Y888 888 888     "Y88888 '
 Write-Host -ForegroundColor White '                                                                        888                                                                   888 '
-Write-Host -ForegroundColor White 'Compiled by nwjohns101                                             Y8b d88P                                                              Y8b d88P '
-Write-Host -ForegroundColor White '                                                                    "Y88P"                                                                "Y88P"  '
+Write-Host -ForegroundColor White 'Author: Cyb3rN8TE 2023                                             Y8b d88P                                                              Y8b d88P '
+Write-Host -ForegroundColor White 'Version: 1.0.0                                                      "Y88P"                                                                "Y88P"  '
 #Banner end
 
 # Switch statement that detects which operating system is being used (Windows or Mac OS)
